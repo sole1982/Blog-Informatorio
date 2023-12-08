b8504e46add53db30dc2b3f5c58ba5fd9d93a8dd
sole1982 <soleadhernandez@gmail.com>
2023-12-08 15:21:06 -0300

    vista de imagenes en post

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 # Create your views here.
-def posts(request):
-    posts = Post.objects.all()
-    return render(request,'posts.html', {'posts' : posts}  )
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post/posts.html"
+    context_object_name = "posts"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post/post_individual.html"
+    context_object_name = "posts"
+    pk_url_kwarg = "id"
+    queryset = Post.objects.all()
+

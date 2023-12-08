4fcd1e8913e4d7e51e5242aedddbbc664300220c
sole1982 <soleadhernandez@gmail.com>
2023-12-08 12:26:25 -0300

    vista de post

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def posts(request):
+    posts = Post.objects.all()
+    return render(request,'posts.html', {'posts' : posts}  )

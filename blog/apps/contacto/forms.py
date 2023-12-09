b192961d4ce0f854ab5bb4eb1cf78abd584335fa
JoseArielGimenezVargas <arielgimenezvargas@gmail.com>
2023-12-09 14:17:35 -0300

    Agregué estilo bs5 al formulario de Contacto

--- blog/apps/contacto/forms.py
+++ blog/apps/contacto/forms.py
@@ -6,3 +6,9 @@ class ContactoForm(forms.ModelForm):
     class Meta:
         model = Contacto
         fields = ['nombre_apellido', 'email', 'asunto', 'mensaje']
+        widgets = {
+            'nombre_apellido': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'asunto': forms.TextInput(attrs={'class': 'form-control'}),
+            'mensaje': forms.Textarea(attrs={'class': 'form-control'}),
+        }

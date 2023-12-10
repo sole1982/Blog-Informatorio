b141a201d604632a0de9e70f118961ae162c0a0c
sole1982 <soleadhernandez@gmail.com>
2023-12-10 11:17:12 -0300

    cambios en contacto

--- blog/apps/contacto/models.py
+++ blog/apps/contacto/models.py
@@ -4,12 +4,12 @@ from django.utils import timezone
 # Create your models here.
 
 class Contacto(models.Model):
-    nombre = models.CharField(max_length=120)
-    apellido = models.CharField(max_length=120)
+    nombre_apellido = models.CharField(max_length=120)
+    
     email = models.EmailField()
-    consulta = models.CharField(max_length=50)
+    asunto = models.CharField(max_length=50)
     mensaje = models.TextField()
     fecha = models.DateTimeField(default=timezone.now)
 
     def __str__(self) -> str:
-        return self.nombre
+        return self.nombre_apellido

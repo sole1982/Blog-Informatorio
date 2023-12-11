5182468a853358056e55bba773a70304b653d471
JoseArielGimenezVargas <arielgimenezvargas@gmail.com>
2023-12-11 01:37:00 -0300

    reestablezco superusuario para usar MySql

--- blog/blog/configuraciones/settings.py
+++ blog/blog/configuraciones/settings.py
@@ -27,9 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   'apps.usuario',
-   'apps.posts',
-   'apps.contacto',
+    
+    'apps.usuario',
+    'apps.posts',
+    'apps.contacto',
    
 ]
 

1ec6237c7e409cda8e5149a254303704cb672bfb
sole1982 <soleadhernandez@gmail.com>
2023-12-12 10:21:57 -0300

    cambio base de dato

--- blog/blog/configuraciones/settings.py
+++ blog/blog/configuraciones/settings.py
@@ -68,7 +68,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
 
+
+    }}
 
 
 # Password validation

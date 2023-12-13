1c557543fc50d69ec7f7ee42c3c3cc3777a8e967
JoseArielGimenezVargas <arielgimenezvargas@gmail.com>
2023-12-13 02:11:05 -0300

    Reestablezco el link a Contacto

--- blog/apps/usuario/views.py
+++ blog/apps/usuario/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from .forms import RegistroUsuarioForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
 
 # Create your views here.
+
+class RegistrarUsuario(CreateView):
+    template_name = 'registration/register.html'
+    form_class = RegistroUsuarioForm
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Registro exitoso. Por favor, inicia sesión.')
+        form.save()
+
+        return redirect('apps.usuario:registrar')
+
+class LoginUsuario(LoginView):
+    template_name = 'registration/login.html'
+
+    def get_success_url(self):
+        messages.success(self.request, 'Login exitoso.')
+
+        return reverse('apps.usuario:login')
+    
+
+class LogoutUsuario(LogoutView):
+    template_name = 'registration/logout.html'
+
+    def get_success_url(self):
+        messages.success(self.request, 'Logout exitoso.')
+
+        return reverse('apps.usuario:logout')
+    
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -8,15 +8,19 @@ from .views import index
 from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from apps.usuario.views import RegistrarUsuario, LoginUsuario, LogoutUsuario 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-path('', include('apps.posts.urls')),
-path('', include('apps.contacto.urls')),
+    path('', include('apps.posts.urls')),
+    path('', include('apps.contacto.urls')),
+    path('registrar/', RegistrarUsuario.as_view(), name='registrar'),  
+    path('login/', LoginUsuario.as_view(), name='login'),  
+    path('logout/', LogoutUsuario.as_view(), name='logout'),  
+    
 
 ]  +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 urlpatterns += staticfiles_urlpatterns()

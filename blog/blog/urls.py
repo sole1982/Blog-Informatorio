bb64667aa0601383030e8535a457e975d82a1439
JoseArielGimenezVargas <arielgimenezvargas@gmail.com>
2023-12-14 01:35:01 -0300

    form-view-url-template para usuarios hasta pag 81

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path, include
 from .views import index
 from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from apps.usuario.views import RegistrarUsuario, LoginUsuario, LogoutUsuario 
+
 
 
 
@@ -17,9 +13,8 @@ urlpatterns = [
     path('', index, name='index'),
     path('', include('apps.posts.urls')),
     path('', include('apps.contacto.urls')),
-    path('registrar/', RegistrarUsuario.as_view(), name='registrar'),  
-    path('login/', LoginUsuario.as_view(), name='login'),  
-    path('logout/', LogoutUsuario.as_view(), name='logout'),  
+    path('', include('apps.usuario.urls')),
+    path('', include('django.contrib.auth.urls')), 
     
 
 ]  +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
